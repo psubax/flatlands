94d91c09ba06d04b840deb0f96355cb3516bda01
pauliuss <paulius.subacius@brolis-sensor.com>
2023-11-23 17:08:08 +0200

    Changed model params

--- flatlands_model/model.py
+++ flatlands_model/model.py
@@ -3,20 +3,21 @@ import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Dense, Input, Flatten, Conv2D, BatchNormalization, \
-                                    Activation, Dropout, MaxPooling2D
+    Activation, Dropout, MaxPooling2D
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.metrics import SparseCategoricalAccuracy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import TensorBoard
-
 
 data = np.load('flatland_train.npz')
 X = data['X']
 y = data['y']
 
-y[y != 0] -= 2    # Correct labels so that triangle is mapped to class 1
-X = X / 255.      # Scale down to range [0, 1]
+y[y != 0] -= 2  # Correct labels so that triangle is mapped to class 1
+X = X / 255.  # Scale down to range [0, 1]
 
 # Construct and train your model (don't forget train/test split and other tricks)
 # model = ...
@@ -26,22 +27,23 @@ X = X / 255.      # Scale down to range [0, 1]
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5), padding='same', activation='relu', input_shape=[50, 50, 1]))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.1))
 model.add(Conv2D(16, (5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(4, 4)))
-model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.1))
 model.add(Flatten())
 model.add(Dense(26, activation='relu'))
 model.add(Dense(5, activation='softmax'))
 model.compile(loss="sparse_categorical_crossentropy",
               optimizer=keras.optimizers.Adam(),
-              metrics=["accuracy"])
+              metrics=[SparseCategoricalAccuracy()])
 model.summary()
 
 loss = model.fit(X,
-                y,
-                epochs=50,
-                batch_size=64,
-                validation_split=0.2)
+                 y,
+                 epochs=13,
+                 batch_size=64,
+                 validation_split=0.2)
 
 model.save('model.h5')
-
