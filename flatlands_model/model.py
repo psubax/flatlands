6bfe37d08ad176a13db0ea53f98cd8ba8f77e2df
pauliuss <paulius.subacius@brolis-sensor.com>
2023-11-24 11:08:32 +0200

    Changed model params

--- flatlands_model/model.py
+++ flatlands_model/model.py
@@ -10,6 +10,9 @@ from keras.models import Sequential
 from keras.metrics import SparseCategoricalAccuracy
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard
+from PIL import Image
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
 
 data = np.load('flatland_train.npz')
 X = data['X']
@@ -24,9 +27,22 @@ X = X / 255.  # Scale down to range [0, 1]
 # Save the model and upload it to git
 # model.save('model.h5')
 
+
+# Specify the amount of padding for each side (left, top, right, bottom)
+padding = (11, 11, 11, 11)
+
+X_padded = np.zeros([X.shape[0], 72, 72])
+
+for i in range(X.shape[0]):
+    padded_image = np.pad(X[i, :, :], ((padding[1], padding[3]), (padding[0], padding[2])), mode='constant')
+    X_padded[i, :, :] = padded_image
+
+X = X_padded
+X = np.expand_dims(X, (-1))
+
 model = Sequential()
-model.add(ZeroPadding2D(padding=11, input_shape=[50, 50, 1]))
-model.add(RandomRotation(factor=np.pi/2, fill_mode='constant', fill_value=0))
+# model.add(ZeroPadding2D(padding=11, input_shape=[50, 50, 1]))
+# model.add(RandomRotation(factor=np.pi / 2, fill_mode='constant', fill_value=0))
 model.add(Conv2D(8, kernel_size=(3, 3), padding='same', activation='relu', input_shape=[72, 72, 1]))
 # model.add(MaxPooling2D(pool_size=(2, 2)))
 # model.add(Dropout(0.1))
@@ -46,10 +62,31 @@ model.compile(loss="sparse_categorical_crossentropy",
               metrics=[SparseCategoricalAccuracy()])
 model.summary()
 
-loss = model.fit(X,
-                 y,
-                 epochs=50,
-                 batch_size=128,
-                 validation_split=0.2)
+# Create an instance of ImageDataGenerator for data augmentation
+datagen = ImageDataGenerator(
+    rotation_range=90,
+    shear_range=0.2,
+    zoom_range=0.2,
+    # horizontal_flip=True
+)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create a generator for reading images from the specified directory
+train_generator = datagen.flow(
+    X_train,
+    y_train,
+    batch_size=256
+)
+
+# Train your model using the generator
+model.fit(train_generator, epochs=50, batch_size=256)
+pred = model.predict(X_test).argmax(axis=1)
+print('Accuracy on test set - {0:.02%}'.format((pred == y_test).mean()))
+# loss = model.fit(X,
+#                  y,
+#                  epochs=50,
+#                  batch_size=128,
+#                  validation_split=0.2)
 
 model.save('model.h5')
